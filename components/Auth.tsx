4fb75b360b49f9d523f431852f1b44fbdabd0580
martinromario55 <martinromario55@gmail.com>
2024-10-12 23:50:50 +0300

    Hook

--- components/Auth.tsx
+++ components/Auth.tsx
@@ -1,45 +1,22 @@
-import Ionicons from '@expo/vector-icons/Ionicons'
-import { PropsWithChildren, useState } from 'react'
-import { StyleSheet, TouchableOpacity, useColorScheme } from 'react-native'
+/**
+ * Learn more about light and dark modes:
+ * https://docs.expo.dev/guides/color-schemes/
+ */
 
-import { ThemedText } from '@/components/ThemedText'
-import { ThemedView } from '@/components/ThemedView'
+import { useColorScheme } from 'react-native'
+
 import { Colors } from '@/constants/Colors'
 
-export function Collapsible({
-  children,
-  title,
-}: PropsWithChildren & { title: string }) {
-  const [isOpen, setIsOpen] = useState(false)
+export function useThemeColor(
+  props: { light?: string; dark?: string },
+  colorName: keyof typeof Colors.light & keyof typeof Colors.dark
+) {
   const theme = useColorScheme() ?? 'light'
+  const colorFromProps = props[theme]
 
-  return (
-    <ThemedView>
-      <TouchableOpacity
-        style={styles.heading}
-        onPress={() => setIsOpen(value => !value)}
-        activeOpacity={0.8}
-      >
-        <Ionicons
-          name={isOpen ? 'chevron-down' : 'chevron-forward-outline'}
-          size={18}
-          color={theme === 'light' ? Colors.light.icon : Colors.dark.icon}
-        />
-        <ThemedText type="defaultSemiBold">{title}</ThemedText>
-      </TouchableOpacity>
-      {isOpen && <ThemedView style={styles.content}>{children}</ThemedView>}
-    </ThemedView>
-  )
+  if (colorFromProps) {
+    return colorFromProps
+  } else {
+    return Colors[theme][colorName]
+  }
 }
-
-const styles = StyleSheet.create({
-  heading: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 6,
-  },
-  content: {
-    marginTop: 6,
-    marginLeft: 24,
-  },
-})
